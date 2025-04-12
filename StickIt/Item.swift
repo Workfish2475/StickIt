9b8f0b5825709f255ffc3936077aa06dc7c74367
Alex <78886791+alexander2475@users.noreply.github.com>
2025-04-12 18:49:09 -0500

    Initial Commit

---
+++ StickIt/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  StickIt
+//
+//  Created by Alexander Rivera on 4/12/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ StickItTests/StickItTests.swift
@@ -0,0 +1,16 @@
+//
+//  StickItTests.swift
+//  StickItTests
+//
+//  Created by Alexander Rivera on 4/12/25.
+//
+
+import Testing
+
+struct StickItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
